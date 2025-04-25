989356905a289553e36581c37c17a8a04ee02f62
Nihal Panday <nihalpanday2020@gmail.com>
2025-04-25 09:14:24 +0530

    Intial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CampaignDashboard from './components/CampaignDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <CampaignDashboard />
+    </div>
+  );
+}
+
+export default App;
